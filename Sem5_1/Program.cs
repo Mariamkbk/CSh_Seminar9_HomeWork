bd87477d5613b40f8bc8bde69676dcb4fa7a394e
Masulka <kuzmenkomkbk@gmail.com>
2022-12-04 12:30:19 +0300

    save tasks from seminar 5

--- Sem5_1/Program.cs
+++ Sem5_1/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Задача 32: Напишите программу замена элементов массива: 
+// положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-9, 10);
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+
+void Zamena(int[] array)
+{
+    
+    for (int i = 0; i < array.Length; i++ )
+    {
+        array[i] *= -1; 
+                
+    }
+    
+}   
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = CreateArray(size);
+PrintArray(array);
+Console.WriteLine("Вот измененный массив");
+Zamena(array);
+PrintArray(array);
--- Sem5_2/Program.cs
+++ Sem5_2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+bool FindNum(int[] array, int num)
+{
+    bool res = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == num)
+        {
+            res = true;
+            break;
+        }
+    }
+    return res;
+}
+
+Console.WriteLine("Введите размерность массива");
+int size = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите искомое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = CreateArray(size);
+PrintArray(array);
+if (FindNum(array, num)) Console.WriteLine("ДА, Число присутствует в массиве!");
+else Console.WriteLine("НЕТ! Искомого числа");
