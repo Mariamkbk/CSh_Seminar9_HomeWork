8eab9a0129cdeb6a41150cf48b47e4971567d8e4
Masulka <kuzmenkomkbk@gmail.com>
2022-12-11 12:22:29 +0300

    task 47 started

--- Sem7_47/Program.cs
+++ Sem7_47/Program.cs
@@ -5,3 +5,22 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
+void FillAndShowArray(int horizontal, int vertical)
+{
+    double[,] Array = new double[horizontal, vertical];
+    for (int i = 0; i < vertical; i++)
+    {
+        for (int j = 0; j < horizontal; j++)
+        {
+            Array[j, i] = new Random().NextDouble()*10;
+            Console.Write($"{Array[j, i]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк в таблице");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов в таблице");
+int n = Convert.ToInt32(Console.ReadLine());
+FillAndShowArray(m, n);
