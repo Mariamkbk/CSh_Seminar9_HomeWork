e48bdda056157b29f74ef131e89de76def2eb1d2
Masulka <kuzmenkomkbk@gmail.com>
2022-12-18 00:43:39 +0300

    task 62 is Ok with uneven

--- Sem8_62/Program.cs
+++ Sem8_62/Program.cs
@@ -5,4 +5,84 @@
 // 11 16 15 06
 // 10 09 08 07
 
+int[,] FillArray(int size)
+{
+    int[,] Array = new int[size, size];
+    int number = 1;
+    int i = 0;
+    int j = 0;
+    int leftWall = 0;
+    int upperWall = 1;
+    while (j <= size - 1)
+    {
+        if (j != size - 1)
+        {
+            Array[j, i] = number++;
+            j++;
+        }
+        else if (upperWall >= size) j++;
+        
+        else
+        {
+            while (i <= size - 1 && j >= leftWall)
+            {
+                if (i != size - 1)
+                {
+                    Array[j, i] = number++;
+                    i++;
+                }
+                else
+                {
+                    size--;
+                    while (j >= leftWall)
+                    {
+                        if (j != leftWall)
+                        {
+                            Array[j, i] = number++;
+                            j--;
+                        }
+                        else
+                        {
+                            leftWall++;
+                            while (i >= upperWall)
+                            {
+                                if (i != upperWall)
+                                {
+                                    Array[j, i] = number++;
+                                    i--;
+                                }
+                                else
+                                    upperWall++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    Array[j, i] = number++;
+    return Array;
+}
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            Console.Write($"{arr[j, i]}\t");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
+// try
+// {
+Console.WriteLine("Введите число соответствующее количеству столбцов и строк в массиве");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintArray(FillArray(n));
+// }
+// catch
+// {
+//     Console.WriteLine("Проверьте правильность введенных данных");
+// }
