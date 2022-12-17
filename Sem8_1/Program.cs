ddb47c1720d98ca6692651c7698bf4f171615b8b
Masulka <kuzmenkomkbk@gmail.com>
2022-12-17 12:35:10 +0300

    new tasks

--- Sem8_1/Program.cs
+++ Sem8_1/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+void FillArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+       for (int j=0;j<array.GetLength(1);j++) 
+            array[i,j] = new Random().Next(-10,10);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i=0;i<array.GetLength(0);i++)
+       {
+        for (int j=0;j<array.GetLength(1);j++) 
+            Console.Write($"{array[i,j],2}\t");
+        Console.WriteLine();
+       }
+}
+
+void ChangeArray(int [,] array)
+{
+for (int j=0;j<array.GetLength(1);j++)
+
+ (array[0,j], array[array.GetLength(0)-1,j])=(array[array.GetLength(0)-1,j],array[0,j]);
+
+}
+
+
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int [m, n];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
--- Sem8_2/Program.cs
+++ Sem8_2/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(0, 21);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        Console.WriteLine();
+    }
+}
+
+void CountNumInArray(int[,] array)
+{
+    int[] tempArray = new int[21];
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            tempArray[array[i, j]] += 1;
+    for (int k = 0; k < 21; k++)
+    {
+        if (tempArray[k] != 0)
+            Console.WriteLine($"Число {k} встречается {tempArray[k]} раз");
+    }
+}
+
+Console.WriteLine("Ведите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+CountNumInArray(array);
+
