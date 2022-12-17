a5c19dc72f330c0ee800c8426f5762c2a0852c2b
Masulka <kuzmenkomkbk@gmail.com>
2022-12-17 13:55:44 +0300

    task 54 finished

--- Sem8_54/Program.cs
+++ Sem8_54/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 54: Задайте двумерный массив. 
+﻿// Задача 54: Задайте двумерный массив.
 // Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 // Например, задан массив:
 // 1 4 7 2
@@ -9,40 +9,78 @@
 // 9 5 3 2
 // 8 4 4 2
 
-void FillArray(int[,] array)
+int[,] FillArray(int horizontal, int vertical)
 {
-    for (int i=0;i<array.GetLength(0);i++)
-       for (int j=0;j<array.GetLength(1);j++) 
-            array[i,j] = new Random().Next(-10,10);
+    int[,] Array = new int[horizontal, vertical];
+    for (int i = 0; i < vertical; i++)
+    {
+        for (int j = 0; j < horizontal; j++)
+        {
+            Array[j, i] = new Random().Next(1, 20);
+        }
+    }
+    return Array;
 }
-
-void PrintArray(int[,] array)
+void PrintArray(int[,] arr)
 {
-    for (int i=0;i<array.GetLength(0);i++)
-       {
-        for (int j=0;j<array.GetLength(1);j++) 
-            Console.Write($"{array[i,j],2}\t");
+    for (int i = 0; i < arr.GetLength(1); i++)
+    {
+        for (int j = 0; j < arr.GetLength(0); j++)
+        {
+            Console.Write($"{arr[j, i]}\t");
+        }
         Console.WriteLine();
-       }
+    }
+    Console.WriteLine();
 }
 
-void ChangeArray(int [,] array)
+int[,] ArrangeEveryString(int[,] arr)
 {
-for (int j=0;j<array.GetLength(1);j++)
-
- (array[0,j], array[array.GetLength(0)-1,j])=(array[array.GetLength(0)-1,j],array[0,j]);
-
+    int i = 0;
+    int j = 0;
+    int save = 0;
+    int changePointi = 0;
+    int changePointj = 0;
+    while (i < arr.GetLength(1))
+    {
+        while (changePointj < arr.GetLength(0))
+        {
+            int max = arr[j, i];
+            while (j < arr.GetLength(0))
+            {
+                if (arr[j, i] > max)
+                {
+                    max = arr[j, i];
+                    save = arr[changePointj, changePointi];
+                    arr[changePointj, changePointi] = max;
+                    arr[j, i] = save;
+                }
+                j++;
+            }
+            changePointj++;
+            j = changePointj;
+        }
+        changePointi++;
+        i = changePointi;
+        changePointj = 0;
+        j = changePointj;
+    }
+    return arr;
 }
-
-
-
-Console.WriteLine("Введите количество строк двумерного массива");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов двумерного массива");
-int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int [m, n];
-FillArray(array);
-PrintArray(array);
-Console.WriteLine();
-ChangeArray(array);
-PrintArray(array);
+try
+{
+    Console.WriteLine("Введите количество строк в массиве");
+    int m = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов в массиве");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[,] array = FillArray(m, n);
+    Console.WriteLine("Массив создан: ");
+    PrintArray(array);
+    ArrangeEveryString(array);
+    Console.WriteLine("Массив отсортирован: ");
+    PrintArray(array);
+}
+catch
+{
+    Console.WriteLine("Проверьте правильность введенных данных");
+}
